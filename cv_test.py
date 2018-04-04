8ea048c7e3cf6e2e844e58d632f72c67c889c38c
Ctacus <ctacus@gmail.com>
2018-04-04 23:11:28 +0400

    small refactoring and simple scaling prepared

--- cv_test.py
+++ cv_test.py
@@ -4,6 +4,15 @@ import random
 
 from matplotlib import pyplot as plt
 
+methods = [
+    # {'color': (0, 0, 255), 'name': 'cv2.TM_CCOEFF'},
+    {'color': (100, 150, 200), 'name': 'cv2.TM_CCOEFF_NORMED'},
+    # {'color': (0, 255, 0), 'name': 'cv2.TM_CCORR'},
+    # {'color': (0, 150, 0), 'name': 'cv2.TM_CCORR_NORMED'},
+    # {'color': (255, 0, 0), 'name': 'cv2.TM_SQDIFF'},
+    # {'color': (150, 0, 0), 'name': 'cv2.TM_SQDIFF_NORMED'}
+]
+
 
 def rect(img, top_left, bottom_right, color, texts):
     cv2.rectangle(img, top_left, bottom_right, color, 2)
@@ -28,18 +37,28 @@ def get_boundaries(match_res, method, w, h):
     return top_left, bottom_right, max_val
 
 
-def match(img, templates):
-    img2 = img.copy()
+def match(img, template, method, w, h):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    res = cv2.matchTemplate(gray, template[1], method)
+    return get_boundaries(res, method, w, h)
 
-    # All the 6 methods for comparison in a list
-    methods = [
-        # {'color': (0, 0, 255), 'name': 'cv2.TM_CCOEFF'},
-        {'color': (100, 150, 200), 'name': 'cv2.TM_CCOEFF_NORMED'},
-        # {'color': (0, 255, 0), 'name': 'cv2.TM_CCORR'},
-        # {'color': (0, 150, 0), 'name': 'cv2.TM_CCORR_NORMED'},
-        # {'color': (255, 0, 0), 'name': 'cv2.TM_SQDIFF'},
-        # {'color': (150, 0, 0), 'name': 'cv2.TM_SQDIFF_NORMED'}
-    ]
+
+def max_scaled_math(img, template, max_downscale, max_steps, method):
+    w, h = template[1].shape[::-1]
+    step = (1 - max_downscale) / max_steps
+    scale = 1
+    best = (0, 0, 0, 1)
+    max = 0
+    for n in range(1, max_steps):
+        copy = cv2.resize(img, None, fx=scale, fy=scale)
+        t, b, m = match(copy, template, method, w, h)
+        if m > max:
+            best = (t, b, m, scale)
+        scale = scale-step
+    return best
+
+
+def match_and_draw(img, templates):
     for template in templates:
         w, h = template[1].shape[::-1]
         path = template[0]
@@ -47,15 +66,11 @@ def match(img, templates):
             # copy = img.copy()
             meth_name = meth['name']
             method = eval(meth_name)
-            color = meth['color']
-            # Apply template Matching
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-            res = cv2.matchTemplate(gray, template[1], method)
-            top_left, bottom_right, max = get_boundaries(res, method, w, h)
+            top_left, bottom_right, max = match(img, template, method, w, h)
 
-            if max > 0.5:
-                rect(img, top_left, bottom_right, color,
+            if max > 0.52:
+                rect(img, top_left, bottom_right, meth['color'],
                      ['{path} [{max:.3f}]'.format(path=path, max=max), meth_name[7:]])
 
 
@@ -82,7 +97,7 @@ while (True):
         rot = (rot + 1) % 4
 
     # frame = np.rot90(frame, -rot)
-    match(frame, templates)
+    match_and_draw(frame, templates)
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
